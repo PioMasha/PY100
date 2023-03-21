4638d4d75d0084737d0d5aa23e53eb1083ef846a
Pirogova Maria <mashastrelnikova168@gmail.com>
2023-03-21 22:50:16 +0300

    Py100_Выполнена первая ЛР

--- Тема 2/Лабораторные работы/task1/main.py
+++ Тема 2/Лабораторные работы/task1/main.py
@@ -1,2 +1,9 @@
 list_ = [8, 9, -5, -3, 1, -10, 8, -10, -5, 0, 5, -4, 0, 10, -8, 1, 6, -6, 6, -9]
 # TODO найти сумму, количество и среднее арифметическое уникальных чисел
+unique_list = list(set(list_))
+sum_ = sum(unique_list)
+print(sum_)
+len_ = len(unique_list)
+print(len_)
+round_ = round((sum_ / len_), 5)
+print(round_)
